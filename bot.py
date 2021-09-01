04207fbb57706667dc7a012a20305938a9fbe97f
Siddhartha Bose <siddbose97@gmail.com>
2021-09-01 16:45:53 +0800

    does anything work?

--- bot.py
+++ bot.py
@@ -303,7 +303,18 @@ def qFunc(message):
 #####################################################################################
 
 #bot.polling()
+bot = telebot.TeleBot(TOKEN)
+server = Flask(__name__)
 
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
+
+
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_message(message):
+    bot.reply_to(message, message.text)
 
 @server.route('/' + TOKEN, methods=['POST'])
 def getMessage():
