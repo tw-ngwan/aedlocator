20c5fbc9adf9a1dfb2760f3c210e86c4b654c20e
Siddhartha Bose <siddbose97@gmail.com>
2021-09-29 17:34:53 +0800

    defeated

--- bot.py
+++ bot.py
@@ -11,8 +11,8 @@ import telegram
 from time import sleep
 import os
 import geopy.distance
-from telegram import Location, KeyboardButton
-import telegram.ext
+from telegram import Location, KeyboardButton, ReplyKeyboardMarkup
+from telegram.ext import MessageFilter, MessageHandler, ConversationHandler, Filters, CommandHandler, Updater
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, KeyboardButton
 import logging
 
@@ -29,7 +29,7 @@ API_KEY = os.environ.get('TOKEN')
 PORT = int(os.environ.get('PORT', 8443))
 
 # Create an updater object with our API Key
-updater = telegram.ext.Updater(API_KEY)
+updater = Updater(API_KEY)
 # Retrieve the dispatcher, which will be used to add handlers
 dispatcher = updater.dispatcher
 # Our states, as integers
@@ -54,25 +54,25 @@ class AED:
 
 def start(update_obj, context):
     """Send a message when the command /start is issued."""
-    buttons = [[telegram.KeyboardButton(text='Nearest AED',request_location=True)],\
-        [telegram.KeyboardButton(text='Static Map')],[telegram.KeyboardButton(text='Restart')]]
-    kb = telegram.ReplyKeyboardMarkup(keyboard=buttons,resize_keyboard = True, one_time_keyboard = True)
+
+
+    buttons = [[KeyboardButton(text='Nearest AED',request_location=True)],\
+        [KeyboardButton(text='Static Map')],[KeyboardButton(text='Restart')]]
+
+    kb = ReplyKeyboardMarkup(keyboard=buttons,resize_keyboard = True, one_time_keyboard = True)
+
     welcomeString = """
     Hello, would you like to see your nearest AED or a static map?
 If you click Nearest AED, the bot will request your location!
 Click the RESTART button at any time to restart the commands!!
     """
+
     update_obj.message.reply_text(welcomeString,reply_markup=kb)
     return STATECHECKER
     
 def state_checker(update_obj, context):
-    location = update_obj.message.location
-    print(location)
-    
-    print(f""" 
-    type is: {type(update_obj)} 
-    updateobj is: {update_obj}""")
-    return END
+    print("SC func")
+    return ConversationHandler.END
 
 
 
@@ -83,7 +83,7 @@ def cancel(update_obj, context):
     update_obj.message.reply_text(
         f"Update is {update_obj}!", reply_markup=telegram.ReplyKeyboardRemove()
     )
-    return telegram.ext.ConversationHandler.END
+    return ConversationHandler.END
 
 def end(update_obj, context):
     # get the user's first name
@@ -91,21 +91,21 @@ def end(update_obj, context):
     update_obj.message.reply_text(
         f"Thank you {first_name}, have a wonderful day. Press /start to restart the bot!", reply_markup=telegram.ReplyKeyboardRemove()
     )
-    return telegram.ext.ConversationHandler.END
+    return ConversationHandler.END
 
 def main():
 
 
 
-    handler = telegram.ext.ConversationHandler(
-        entry_points=[telegram.ext.CommandHandler('start', start)],
+    handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
         states={
-                STATECHECKER: [telegram.ext.MessageHandler(telegram.ext.Filters.text or telegram.ext.Filters.location  , state_checker)],
-                CANCEL: [telegram.ext.MessageHandler(telegram.ext.Filters.text, cancel)],
-                END: [telegram.ext.MessageHandler(telegram.ext.Filters.text, end)]
+                STATECHECKER: [MessageHandler(Filters.text('Nearest AED' or 'Static Map') or Filters.location  , state_checker)],
+                CANCEL: [MessageHandler(Filters.text, cancel)],
+                END: [MessageHandler(Filters.text, end)]
 
         },
-        fallbacks=[telegram.ext.CommandHandler('end', end)],
+        fallbacks=[CommandHandler('end', end)],
         )
     # add the handler to the dispatcher
     dispatcher.add_handler(handler)
